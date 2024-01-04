7396996330ea4baecfb5bc249606c041d900e256
Ricardo Rosales <728243+missingcharacter@users.noreply.github.com>
2024-01-03 20:39:45 -0600

    Moved config from after into plugins: - fugitive - harpoon - refactoring - telescope - treesitter - undotree - zenmode

--- nvim/lua/plugins/theprimeagen-refactoring.lua
+++ nvim/lua/plugins/theprimeagen-refactoring.lua
@@ -6,5 +6,7 @@ return   {
   },
   config = function()
     require("refactoring").setup()
+
+    vim.api.nvim_set_keymap("v", "<leader>ri", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]], {noremap = true, silent = true, expr = false})
   end,
 }

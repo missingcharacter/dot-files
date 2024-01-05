e594408c6deaa03ce8ded5895b9fe3a962f33fbb
Ricardo Rosales <728243+missingcharacter@users.noreply.github.com>
2024-01-05 16:52:14 -0600

    Removed duplicate `folke/trouble` plugin declaration copied ThePrimeagen trouble and telescope keymaps

--- nvim/lua/plugins/theprimeagen-harpoon.lua
+++ nvim/lua/plugins/theprimeagen-harpoon.lua
@@ -1,5 +1,5 @@
 return {
-  "theprimeagen/harpoon",
+  "ThePrimeagen/harpoon",
   config = function ()
     local mark = require("harpoon.mark")
     local ui = require("harpoon.ui")
--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,13 +1,37 @@
 return {
  "folke/trouble.nvim",
  dependencies = { "nvim-tree/nvim-web-devicons" },
+ opts = {
+  icons = false,
+  -- your configuration comes here
+  -- or leave it empty to use the default settings
+  -- refer to the configuration section below
+ },
  config = function ()
-    -- Lua
-    vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
-    vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
-    vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
-    vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
-    vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
-    vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+    local trouble = require("trouble")
+    vim.keymap.set("n", "<leader>tt", function()
+        trouble.toggle()
+    end)
+    vim.keymap.set("n", "<leader>tw", function()
+        trouble.toggle("workspace_diagnostics")
+    end)
+    vim.keymap.set("n", "<leader>td", function()
+        trouble.toggle("document_diagnostics")
+    end)
+    vim.keymap.set("n", "<leader>tq", function()
+        trouble.toggle("quickfix")
+    end)
+    vim.keymap.set("n", "<leader>tl", function()
+        trouble.toggle("loclist")
+    end)
+    vim.keymap.set("n", "gR", function()
+        trouble.toggle("lsp_references")
+    end)
+    vim.keymap.set('n', '<leader>tn', function()
+        trouble.next({skip_groups = true, jump = true})
+    end)
+    vim.keymap.set('n', '<leader>tp', function()
+        trouble.previous({skip_groups = true, jump = true})
+    end)
  end
 }

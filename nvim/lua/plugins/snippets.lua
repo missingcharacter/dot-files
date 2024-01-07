759057261b8b3ae57880a791021a5202fda57741
Ricardo Rosales <728243+missingcharacter@users.noreply.github.com>
2024-01-07 15:46:39 -0600

    Copied thevimeagen snippet config

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -8,6 +8,17 @@ return {
     dependencies = { "rafamadriz/friendly-snippets" },
 
     config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
+        local ls = require("luasnip")
+        ls.filetype_extend("javascript", { "jsdoc" })
+
+        vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+        vim.keymap.set({"i", "s"}, "<leader>;", function() ls.jump(1) end, {silent = true})
+        vim.keymap.set({"i", "s"}, "<ledaer>,", function() ls.jump(-1) end, {silent = true})
+
+        vim.keymap.set({"i", "s"}, "<C-E>", function()
+            if ls.choice_active() then
+                ls.change_choice(1)
+            end
+        end, {silent = true})
     end,
 }

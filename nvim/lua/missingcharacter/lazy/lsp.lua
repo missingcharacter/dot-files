78022341d6b3459765d38ceeb84c9fedbc5047d4
Ricardo Rosales <728243+missingcharacter@users.noreply.github.com>
2024-01-15 15:06:37 -0600

    Added pylsp config

--- nvim/lua/missingcharacter/lazy/lsp.lua
+++ nvim/lua/missingcharacter/lazy/lsp.lua
@@ -16,6 +16,7 @@ return {
         local cmp = require("cmp")
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
+        local lspconfig = require("lspconfig")
         require("fidget").setup({})
         require("mason").setup()
         require("mason-lspconfig").setup({
@@ -43,15 +44,42 @@ return {
             },
             handlers = {
                 function(server_name)
-                    require("lspconfig")[server_name].setup({
+                    lspconfig[server_name].setup({
                         capabilities = capabilities,
                     })
                 end,
                 ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
                     lspconfig.lua_ls.setup({
                         capabilities = capabilities,
                         settings = { Lua = { diagnostics = { globals = { "vim" } } } },
+                    })
+                end,
+                ["pylsp"] = function()
+                    lspconfig.pylsp.setup({
+                        capabilities = capabilities,
+                        settings = {
+                            pylsp = {
+                                plugins = {
+                                    -- formatter options
+                                    black = { enabled = true },
+                                    autopep8 = { enabled = false },
+                                    yapf = { enabled = false },
+                                    -- linter options
+                                    pylint = { enabled = false, executable = "pylint" },
+                                    pyflakes = { enabled = false },
+                                    pycodestyle = { enabled = false },
+                                    -- type checker
+                                    pylsp_mypy = { enabled = true },
+                                    -- auto-completion options
+                                    jedi_completion = { fuzzy = true },
+                                    -- import sorting
+                                    pyls_isort = { enabled = true },
+                                },
+                            },
+                        },
+                        flags = {
+                            debounce_text_changes = 200,
+                        },
                     })
                 end,
             },

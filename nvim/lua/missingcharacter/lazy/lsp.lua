4735680880f4ca8f76bc6e89f05f45373117fc70
Ricardo Rosales <728243+missingcharacter@users.noreply.github.com>
2024-02-21 22:01:34 -0600

    Added uv and ruff to default python packages Added mojo lsp config

--- nvim/lua/missingcharacter/lazy/lsp.lua
+++ nvim/lua/missingcharacter/lazy/lsp.lua
@@ -18,6 +18,10 @@ return {
         local cmp_lsp = require("cmp_nvim_lsp")
         local capabilities = vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
         local lspconfig = require("lspconfig")
+        -- `mason-lspconfig` does not support mojo
+        lspconfig.mojo.setup({
+            capabilities = capabilities,
+        })
         require("fidget").setup({})
         require("mason").setup()
         require("mason-lspconfig").setup({

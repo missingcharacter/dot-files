eeca91a73630462d25aa9190987677f95747ad23
Ricardo Rosales <728243+missingcharacter@users.noreply.github.com>
2026-04-24 10:38:40 -0500

    Updated `ThePrimeagen/refactoring.nvim` dependencies

--- nvim/lua/missingcharacter/lazy/theprimeagen-refactoring.lua
+++ nvim/lua/missingcharacter/lazy/theprimeagen-refactoring.lua
@@ -1,6 +1,11 @@
 return {
     "ThePrimeagen/refactoring.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-treesitter/nvim-treesitter" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "lewis6991/async.nvim",
+    },
+    lazy = false,
     config = function()
         require("refactoring").setup()
 

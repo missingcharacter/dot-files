51a724c5abb0ee72f1d0603053abef717580694a
Ricardo Rosales <728243+missingcharacter@users.noreply.github.com>
2026-07-20 15:43:26 -0500

    [nvim] Removed overrides for droast

--- nvim/lua/missingcharacter/lazy/droast.lua
+++ nvim/lua/missingcharacter/lazy/droast.lua
@@ -1,10 +1,3 @@
 return {
     "immanuwell/droast.nvim",
-    config = function()
-        require("droast").setup({
-            command = "droast",
-            on_save = true,
-            args = { "--preset", "production" },
-        })
-    end,
 }
